8ff38f94f43c165e8adc8b2419335719386adeef
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2022-09-14 22:48:05 +0200

    Added some comments

--- MyStack/Stack.cs
+++ MyStack/Stack.cs
@@ -4,8 +4,8 @@ namespace MyStack
 {
     public class Stack
     {
-        int _size;
-        int[] _stack;
+        int _size;    //Keeps track of the size of the stack, starting at zero 
+        int[] _stack; //Stack implemented as an array
 
         public Stack(int size)
         {
